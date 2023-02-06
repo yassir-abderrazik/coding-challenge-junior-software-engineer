3c92bcbc65ae95ff56b2c533a4ec85aabcda7dd6
yassir abderrazik <yassirabderrazik@gmail.com>
2023-02-06 11:58:23 +0100

    Create CategoryProduct Model

---
+++ app/Models/CatgoryProduct.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CatgoryProduct extends Model
+{
+    use HasFactory;
+}
