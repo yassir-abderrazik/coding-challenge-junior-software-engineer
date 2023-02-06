38d1cdaadf25e3f80ee59296501922a8de68ac99
yassir abderrazik <yassirabderrazik@gmail.com>
2023-02-06 23:12:07 +0100

    Add router-view app blade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,7 +8,9 @@
 	@vite('resources/css/app.css')
 </head>
 <body>
-    <div id="app"></div>
+    <div id="app">
+		<router-view></router-view>
+	</div>
 	@vite('resources/js/app.js')
 </body>
 </html>
