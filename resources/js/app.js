b34ea8fb2df6e6052079891ee5f94b096b7d75fe
yassir abderrazik <yassirabderrazik@gmail.com>
2023-02-06 07:46:58 +0100

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,16 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+import './bootstrap';
+import { createApp } from 'vue';
+
+
+
+import App from './components/App.vue';
+
+const app = createApp(App);
+
+app.mount('#app');
