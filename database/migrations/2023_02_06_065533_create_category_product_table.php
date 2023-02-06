58b8e1c6409777932bf6d4beeebf0826fb937269
yassir abderrazik <yassirabderrazik@gmail.com>
2023-02-06 08:00:58 +0100

    Create category_product table

--- database/migrations/2023_02_06_065533_create_category_product_table.php
+++ database/migrations/2023_02_06_065533_create_category_product_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('category_product', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
