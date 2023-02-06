cff6a78c91d7a43910e2efd02d6cf93041cdd0cf
yassir abderrazik <yassirabderrazik@gmail.com>
2023-02-07 00:32:44 +0100

    add CategoryProduct

--- app/Models/CategoryProduct.php
+++ app/Models/CategoryProduct.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class CatgeoryProduct extends Model
+class CatgegoryProduct extends Model
 {
     use HasFactory;
 }
