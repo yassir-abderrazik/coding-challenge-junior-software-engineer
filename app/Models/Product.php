1ce3ab31e9fada5425d483e930ac4dae6aaa0d55
yassir abderrazik <yassirabderrazik@gmail.com>
2023-02-06 08:04:00 +0100

    Defining Relationships Catgory Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'image',
+    ];
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
