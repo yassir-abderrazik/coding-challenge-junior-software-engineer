b5551ca370f4cb471cc266c0124f9ef8efee85c3
yassir abderrazik <yassirabderrazik@gmail.com>
2023-02-06 07:58:47 +0100

    Create Category table

--- database/migrations/2023_02_06_065043_create_categories_table.php
+++ database/migrations/2023_02_06_065043_create_categories_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name', '40')->unique();
+            $table->foreignId('parent_category')->nullable()->constrained('categories', 'id');
             $table->timestamps();
         });
     }
