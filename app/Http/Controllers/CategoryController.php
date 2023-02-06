deacd5cd22fbb5d5782c82ecc7c190f4e3e0f057
yassir abderrazik <yassirabderrazik@gmail.com>
2023-02-06 12:32:45 +0100

    create methods Category controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\CreateCategoryRequest;
+use App\Http\Resources\CategoryResource;
+use App\Services\CategoryService;
 
 class CategoryController extends Controller
-{
-    //
+{    
+    public function __construct(
+        private CategoryService $CategoryService
+    ) {
+    }
+
+    public function index()
+    {
+        return CategoryResource::collection($this->CategoryService->getCategories());
+    }
+
+    public function store(CreateCategoryRequest $request)
+    {
+        return $this->CategoryService->createCategory($request->name, $request->parent_category ?? 0);
+    }
+
+    public function destroy($id)
+    {
+        return $this->CategoryService->deleteCategory($id);
+    }
 }
