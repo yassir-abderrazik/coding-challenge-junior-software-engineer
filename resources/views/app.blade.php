cb6f92ae697eeb9e0462cdab7d14818705a0fa27
yassir abderrazik <yassirabderrazik@gmail.com>
2023-02-06 23:12:49 +0100

    Add flowbite tailwindcss CDN

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Junior Software Engineer</title>
-
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.css"  rel="stylesheet" />
 	@vite('resources/css/app.css')
 </head>
 <body>
@@ -12,5 +12,6 @@
 		<router-view></router-view>
 	</div>
 	@vite('resources/js/app.js')
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.js"></script>
 </body>
 </html>
