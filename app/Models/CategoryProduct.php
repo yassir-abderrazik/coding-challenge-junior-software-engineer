fe66f2fc042d00adfa3378bae7f2281c58a0f894
yassir abderrazik <yassirabderrazik@gmail.com>
2023-02-13 17:46:58 +0100

    CategoryProduct model add fillable

--- app/Models/CategoryProduct.php
+++ app/Models/CategoryProduct.php
@@ -5,7 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class CatgegoryProduct extends Model
+class CategoryProduct extends Model
 {
     use HasFactory;
+
+    protected $table = 'category_product';
+    
+    protected $fillable = [
+        'category_id',
+        'product_id',
+    ];
 }
