b4c7275f94533e15d028e1193272fc0d8c9db0cb
yassir abderrazik <yassirabderrazik@gmail.com>
2023-02-13 18:00:52 +0100

    axios default baseURL

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -11,7 +11,7 @@ import 'bootstrap';
 
 import axios from 'axios';
 window.axios = axios;
-
+axios.defaults.baseURL = 'http://localhost:8000/api/v1/';
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
